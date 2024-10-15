0234546eac5a78b5eb1927eb4092dbea66e20c1d
gensokyo-kenken <51988985+gensokyo-kenken@users.noreply.github.com>
2024-10-16 00:21:29 +0900

    リクエスト処理

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,49 @@
 import { ScriptEventCommandMessageAfterEvent, system } from "@minecraft/server"
 
+type Session = {
+  type: "data" | "disconnect"
+  disconnectId: string
+  mime: string
+  data: string[]
+}
+type Header = {
+  response: string,
+  [key: string]: any
+}
+export type LadatpsRequest = {
+  status: number
+  header: Header
+}
+
 export class LadatpsServer{
-  onRequest: (header: {[key: string]: any}, reject: ()=>void)=>any
+  private listenId: string;
+  private session: Map<string, Session> = new Map();
+  private onMessage(event: ScriptEventCommandMessageAfterEvent){
+    if(event.id == this.listenId){
+      let header: Header;
+      try{
+        header = JSON.parse(event.message) as Header;
+        if(typeof header != "object"){
+          console.error(`[${this.listenId}] Invalid Request(Not in JSON format)`);
+          return;
+        }
+        if(typeof header.response == 'undefined'){
+          console.error(`[${this.listenId}] Invalid Request(header.response is not defined)`);
+          return;
+        }
+      }catch(error){
+        if(typeof header != "object"){
+          console.error(`[${this.listenId}] Invalid Request(JSON parsing error)\n`, error);
+          return;
+        }
+      }
+    }
+  }
+  onRequest: (request: LadatpsRequest)=>any;
+
   listen(id: string){
+    this.listenId = id;
+    let namespace = id.split(':')[0];
+    system.afterEvents.scriptEventReceive.subscribe((event)=>{ this.onMessage(event) }, {namespaces: [namespace]});
   }
 }
